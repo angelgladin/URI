a84b3755916a4179132400eebc3b3a7b62d38107
angelgladin <an098@hotmail.com>
2016-11-05 00:08:14 -0600

    Add 1001 problem

---
+++ 1001.py
@@ -0,0 +1,12 @@
+'''
+Extremely Basic
+URI Online Judge | 1001
+https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+'''
+def sum(a, b):
+	return a + b
+
+a = int(input())
+b = int(input())
+
+print('X = ' + str(sum(a, b)))
